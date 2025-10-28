f704ec9420ff331f563d31829e00a69d12c5a2fd
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-10-28 15:29:11 +0100

    Dodanie Grid

--- src/RealWorld/Models/Product.cs
+++ src/RealWorld/Models/Product.cs
@@ -5,9 +5,4 @@ public class Product : BaseEntity
     public string Name { get; set; }
     public decimal Price { get; set; }
     public int Quantity { get; set; }
-
-    public override string ToString()
-    {
-        return Name + " " + Price + " " + Quantity;
-    }
 }

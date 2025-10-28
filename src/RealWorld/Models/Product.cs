58e11fb71b756cf9e63e342e1e25889e4c6fc849
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-10-28 16:03:22 +0100

    Dodanie ProductTemplate do zasobow

--- src/RealWorld/Models/Product.cs
+++ src/RealWorld/Models/Product.cs
@@ -5,4 +5,5 @@ public class Product : BaseEntity
     public string Name { get; set; }
     public decimal Price { get; set; }
     public int Quantity { get; set; }
+    public decimal TotalAmount => Price * Quantity;
 }
